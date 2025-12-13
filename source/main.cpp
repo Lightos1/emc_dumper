9c1051e817a9bd913b745dad9d50b2c4fe64d17c
Lightos1 <124387232+Lightos1@users.noreply.github.com>
2025-12-13 23:42:54 +0100

    format

--- source/main.cpp
+++ source/main.cpp
@@ -18,14 +18,14 @@ extern "C" {
     char nx_inner_heap[INNER_HEAP_SIZE];
 
     void __libnx_initheap(void) {
-        void* addr = nx_inner_heap;
+        void *addr = nx_inner_heap;
         size_t size = nx_inner_heap_size;
 
-        extern char* fake_heap_start;
-        extern char* fake_heap_end;
+        extern char *fake_heap_start;
+        extern char *fake_heap_end;
 
-        fake_heap_start = (char*)addr;
-        fake_heap_end = (char*)addr + size;
+        fake_heap_start = (char*) addr;
+        fake_heap_end = (char*) addr + size;
     }
 
     void __appInit(void) {
@@ -116,4 +116,5 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
 
