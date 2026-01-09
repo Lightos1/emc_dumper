5ba6c09d53cefc4f05d0a0575bac207f26c8c02f
Lightos1 <124387232+Lightos1@users.noreply.github.com>
2026-01-10 00:36:07 +0100

    Actually remove all guild files

--- overlay/build/cJSON.d
+++
@@ -1,4 +0,0 @@
-cJSON.o: \
- C:/Users/user/Desktop/hos_dumping/emc_dumper/overlay/libs/libultrahand/common/cJSON.c \
- C:/Users/user/Desktop/hos_dumping/emc_dumper/overlay/libs/libultrahand/common/cJSON.h
-C:/Users/user/Desktop/hos_dumping/emc_dumper/overlay/libs/libultrahand/common/cJSON.h:
--- overlay/build/debug_funcs.d
+++
@@ -1,4 +0,0 @@
-debug_funcs.o: \
- C:/Users/user/Desktop/hos_dumping/emc_dumper/overlay/libs/libultrahand/libultra/source/debug_funcs.cpp \
- C:/Users/user/Desktop/hos_dumping/emc_dumper/overlay/libs/libultrahand/libultra/include/debug_funcs.hpp
-C:/Users/user/Desktop/hos_dumping/emc_dumper/overlay/libs/libultrahand/libultra/include/debug_funcs.hpp:
